a41c11ab17cfe1c981c012fd8f606d5a9c3ca9e1
JonasDamasceno <jonasmichael2021@outlook.com>
2022-12-29 19:37:39 -0300

    fix: reformulação da tabela

--- src/componets/Table.jsx
+++ src/componets/Table.jsx
@@ -1,23 +1,8 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/starWarscontext';
 
-function Table() {
-  const [search, setSearch] = useState('');
-  const head = ['Nome',
-    'Rotation Period',
-    'Orbital Period',
-    'Diameter',
-    'Climate',
-    'Gravity',
-    'Terrain',
-    'Surface Water',
-    'Population',
-    'Filmes',
-    'Created',
-    'Edited',
-    'URL'];
-  const handleChange = ({ target }) => {
-    setSearch(target.value);
-  };
+export default function Table() {
+  const data = useContext(StarWarsContext);
   return (
     <table>
       <thead>
@@ -27,52 +12,39 @@ function Table() {
           <th>Orbital Period</th>
           <th>Diameter</th>
           <th>Climate</th>
-          <th>gravity</th>
-          <th>terrain</th>
-          <th>surface_water</th>
-          <th>population</th>
-          <th>films</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
           <th>Created</th>
           <th>Edited</th>
           <th>URL</th>
         </tr>
-        <input
-          type="number"
-          name="filter-value"
-          data-testid="value-filter"
-          onChange={ handleChange }
-        />
-        <button data-testid="button-filter" type="button">Filtrar</button>
-        <thead>
-          <tr>
-            {head.map((element) => (
-              <th key={ element }>{element}</th>
-            ))}
-          </tr>
-        </thead>
       </thead>
       <tbody>
-        {values.filter((planet) => planet.name.toLowerCase().includes(search))
-          .map((planet) => (
-            <tr key={ planet.name }>
-              <th>{planet.name}</th>
-              <th>{planet.rotation_period}</th>
-              <th>{planet.orbital_period}</th>
-              <th>{planet.diameter}</th>
-              <th>{planet.climate}</th>
-              <th>{planet.gravity}</th>
-              <th>{planet.terrain}</th>
-              <th>{planet.surface_water}</th>
-              <th>{planet.population}</th>
-              <th>{planet.films}</th>
-              <th>{planet.created}</th>
-              <th>{planet.edited}</th>
-              <th>{planet.url}</th>
-            </tr>
-          ))}
+        {data.map((element, index) => (
+          <tr
+            data={ element }
+            key={ index }
+          >
+            <td>{element.name}</td>
+            <td>{element.rotation_period}</td>
+            <td>{element.orbital_period}</td>
+            <td>{element.diameter}</td>
+            <td>{element.climate}</td>
+            <td>{element.gravity}</td>
+            <td>{element.terrain}</td>
+            <td>{element.surface_water}</td>
+            <td>{element.population}</td>
+            <td>
+              {element.films.map((el) => <p key={ el }>{el}</p>)}
+            </td>
+            <td>{element.created}</td>
+            <td>{element.edited}</td>
+            <td>{element.url}</td>
+          </tr>))}
       </tbody>
     </table>
   );
 }
-
-export default Table;
