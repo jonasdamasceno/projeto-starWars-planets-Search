2cdafbc660110633810e6d917707ff5dd5b0000c
jonasdamasceno <jonasmichael2021@outlook.com>
2023-01-11 12:22:13 -0300

    primeira tentativa do requisito 2

--- src/componets/Table.jsx
+++ src/componets/Table.jsx
@@ -1,50 +1,75 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import StarWarsContext from '../context/starWarscontext';
 
 export default function Table() {
   const data = useContext(StarWarsContext);
+  const [planetGuide, setplanetguide] = useState();
+  const [search, setSearch] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setplanetguide(data);
+    if (search.length > 0) {
+      const searchList = planetGuide.filter((element) => element.name.includes(search));
+      setList(searchList);
+    } else {
+      setList(planetGuide);
+    }
+  }, [data, search, planetGuide]);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((element, index) => (
-          <tr
-            data={ element }
-            key={ index }
-          >
-            <td>{element.name}</td>
-            <td>{element.rotation_period}</td>
-            <td>{element.orbital_period}</td>
-            <td>{element.diameter}</td>
-            <td>{element.climate}</td>
-            <td>{element.gravity}</td>
-            <td>{element.terrain}</td>
-            <td>{element.surface_water}</td>
-            <td>{element.population}</td>
-            <td>
-              {element.films.map((el) => <p key={ el }>{el}</p>)}
-            </td>
-            <td>{element.created}</td>
-            <td>{element.edited}</td>
-            <td>{element.url}</td>
-          </tr>))}
-      </tbody>
-    </table>
+    <div>
+      <label htmlFor="mainFilter">
+        <input
+          type="text"
+          data-testid="name-filter"
+          id="mainFilter"
+          onChange={ ({ target }) => setSearch(target.value) }
+          value={ search }
+        />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((element, index) => (
+            <tr
+              data={ element }
+              key={ index }
+            >
+              <td>{element.name}</td>
+              <td>{element.rotation_period}</td>
+              <td>{element.orbital_period}</td>
+              <td>{element.diameter}</td>
+              <td>{element.climate}</td>
+              <td>{element.gravity}</td>
+              <td>{element.terrain}</td>
+              <td>{element.surface_water}</td>
+              <td>{element.population}</td>
+              <td>
+                {element.films.map((el) => <p key={ el }>{el}</p>)}
+              </td>
+              <td>{element.created}</td>
+              <td>{element.edited}</td>
+              <td>{element.url}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    </div>
   );
 }
