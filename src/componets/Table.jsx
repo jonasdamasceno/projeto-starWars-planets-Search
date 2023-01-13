97b344053435b95ebb8ad9375792eb76e13244e3
jonasdamasceno <jonasmichael2021@outlook.com>
2023-01-13 20:45:54 -0300

    feat: finalização do requisito 6

--- src/componets/Table.jsx
+++ src/componets/Table.jsx
@@ -74,11 +74,7 @@ export default function Table() {
           data-testid="column-filter"
           onChange={ handleChange }
         >
-          <option value="population">population</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-          <option value="surface_water">surface_water</option>
+          { filterAttributesOp.map((el, i) => <option el={ el } key={ i }>{el}</option>)}
         </select>
 
         <select
